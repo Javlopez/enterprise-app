f84ebd9f73d9ca59c5c6894a372c53e1e1051400
Javier Lopez Lopez <sjavierlopez@gmail.com>
2019-02-25 17:21:38 -0600

    Add copy all script

---
+++ copyall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export GOOS=linux
+export CGO_ENABLED=0
+
+cd accountservice;go get;go build -o accountservice-linux-amd64;echo built `pwd`;cd ..
+export GOOS=darwin
+
+docker build -t javlopez/accountservice accountservice/
+
+docker service rm accountservice
+docker service create --name=accountservice --replicas=1 --network=my_network -p=6767:6767 javlopez/accountservice
